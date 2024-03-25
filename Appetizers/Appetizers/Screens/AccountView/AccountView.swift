09fbd2dfbf9aee4e4214f36866754c8761941a8e
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-03-25 18:07:43 +0200

    create basic account view

--- Appetizers/Appetizers/Screens/AccountView/AccountView.swift
+++ Appetizers/Appetizers/Screens/AccountView/AccountView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct AccountView: View {
+
+    @State private var firstName = ""
+    @State private var lasttName = ""
+    @State private var email = ""
+    @State private var birthdate = Date()
+    @State private var extraNapkins = false
+    @State private var frequentRefills = false
+
     var body: some View {
         NavigationView {
-            Text("Account")
+            Form {
+                Section {
+                    TextField("First Name", text: $firstName)
+                    TextField("Last Name", text: $lasttName)
+                    TextField("Email", text: $email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.none)
+                        .autocorrectionDisabled(true)
+                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+
+                    Button {
+
+                    } label: {
+                        Text("Save Changes")
+                    }
+                } header: {
+                    Text("Personal Info")
+                }
+
+                Section {
+                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                } header: {
+                    Text("Requests")
+                }
+                .tint(.braindPrimary)
+
+            }
+
                 .navigationTitle("🥗 Account")
         }
     }
