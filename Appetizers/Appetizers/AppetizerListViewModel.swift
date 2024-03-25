d4cd91595116b7c38311809d97603b42ceaff6c3
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-03-25 07:44:10 +0200

    make alert published and connect with the view

--- Appetizers/Appetizers/AppetizerListViewModel.swift
+++ Appetizers/Appetizers/AppetizerListViewModel.swift
@@ -8,16 +8,27 @@
 import SwiftUI
 
 final class AppetizerListViewModel: ObservableObject {
+
+    @Published var alertItem: AlertItem?
     @Published var appetizers: [Appetizer] = []
 
     func getAppetizers() {
-        NetworkManager.shared.getAppetizers { result in
+        NetworkManager.shared.getAppetizers { [self] result in
             DispatchQueue.main.async {
                 switch result {
                 case .success(let appetizers):
                     self.appetizers = appetizers
-                case .failure(let failure):
-                    print(failure.localizedDescription)
+                case .failure(let error):
+                    switch error {
+                    case .invalidResponse:
+                        self.alertItem = AlertContext.invalidResponse
+                    case .invalidURL:
+                        self.alertItem = AlertContext.invalidURL
+                    case .invalidData:
+                        self.alertItem = AlertContext.invalidData
+                    case .unableToComplete:
+                        self.alertItem = AlertContext.unableToComplete
+                    }
                 }
             }
         }
